4c742d6964b4730a4b121d6e44fdd530c7ab23ec
Stefan Bischof <stbischof@bipolis.org>
2025-08-13 12:17:29 +0200

    extract dockerjava in extra module and use in testcontainer
    Signed-off-by: Stefan Bischof <stbischof@bipolis.org>

---
+++ dockerjava/src/test/java/org/eclipse/daanse/tooling/dockerjava/Test.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2024 Contributors to the Eclipse Foundation.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   SmartCity Jena - initial
+ *   Stefan Bischof (bipolis.org) - initial
+ */
+package org.eclipse.daanse.tooling.dockerjava;
+
+import java.time.Duration;
+
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.api.model.Info;
+import com.github.dockerjava.core.DefaultDockerClientConfig;
+import com.github.dockerjava.core.DockerClientConfig;
+import com.github.dockerjava.core.DockerClientImpl;
+import com.github.dockerjava.transport.DockerHttpClient;
+import com.github.dockerjava.zerodep.ZerodepDockerHttpClient;
+
+public class Test {
+
+    @org.junit.jupiter.api.Test
+    void testName() throws Exception {
+
+        DockerClientConfig config = DefaultDockerClientConfig.createDefaultConfigBuilder().build();
+        DockerHttpClient client = new ZerodepDockerHttpClient.Builder().dockerHost(config.getDockerHost())
+                .sslConfig(config.getSSLConfig()).maxConnections(100).connectionTimeout(Duration.ofSeconds(30))
+                .responseTimeout(Duration.ofSeconds(45)).build();
+
+        DockerClient dc = DockerClientImpl.getInstance(config, client);
+        dc.pingCmd().exec();
+
+        Info i = dc.infoCmd().exec();
+        System.out.println(i);
+        System.out.println(i);
+    }
+}
