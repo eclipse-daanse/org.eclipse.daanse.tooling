ee718b336453d396d4b4d1a4268f64c4349e7631
dbulahov <bulahovdenis@gmail.com>
2026-02-11 09:00:41 +0300

    fix license header Signed-off-by: dbulahov <bulahovdenis@gmail.com>

--- docgen/core/src/test/resources/examples/SampleDocExample.java
+++ docgen/core/src/test/resources/examples/SampleDocExample.java
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2025 Contributors to the Eclipse Foundation.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
 package examples;
 
 import org.eclipse.daanse.tooling.docgen.api.annotation.DocExample;
@@ -14,14 +23,14 @@ import org.eclipse.daanse.tooling.docgen.api.annotation.DocOutput;
 public class SampleDocExample {
 
     @DocStep(order = 1, title = "First Step",
-             description = "This is the first step.")
+        description = "This is the first step.")
     public void stepOne() {
         String greeting = "Hello";
         System.out.println(greeting);
     }
 
     @DocStep(order = 2, title = "Second Step",
-             description = "This is the second step.")
+        description = "This is the second step.")
     @DocOutput("Result: 42")
     public void stepTwo() {
         int result = 42;
